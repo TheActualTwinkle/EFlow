3971637f8588d5869b384e5b753ecb4e3d8f13cd
fedor <theactualtwinkle@gmail.com>
2025-09-21 02:51:15 +0700

    Refactor DomainTests to exclude specific directories when retrieving csproj files

--- EFlow/Tests/EFlow.ArchitectureTests/Domain/DomainTests.cs
+++ EFlow/Tests/EFlow.ArchitectureTests/Domain/DomainTests.cs
@@ -9,20 +9,23 @@ public class DomainTests
     private const string DomainNamespace = "EFlow.Domain";
     private const string TestNamespace = "EFlow.ArchitectureTests";
 
+    private static readonly string[] DirectoryExclusions = ["scripts"];
+
     [Fact]
-    public void Domain_Should_Not_Have_Project_Dependencies()
+    public void Domain_ShouldNotHaveProjectDependencies()
     {
-        // Arrange & Act
+        // Arrange
         var domainAssembly = typeof(IEntity).Assembly;
 
-        var solutionDir = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "../../../../../.."));
-        var csprojFiles = Directory.GetFiles(solutionDir, "*.csproj", SearchOption.AllDirectories);
+        var solutionDirectory = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "../../../../../.."));
+        var csprojFiles = GetCsprojFilesExcludingDirectories(solutionDirectory);
 
         var forbiddenProjects = csprojFiles
             .Select(Path.GetFileNameWithoutExtension)
             .Where(name => name != DomainNamespace && name != TestNamespace)
             .ToArray();
 
+        // Act
         var result = Types.InAssembly(domainAssembly)
             .ShouldNot()
             .HaveDependencyOnAny(forbiddenProjects)
@@ -30,6 +33,27 @@ public class DomainTests
 
         // Assert
         result.IsSuccessful.Should().BeTrue("Domain should not have dependencies on any other projects");
+    }
+
+    private static IEnumerable<string> GetCsprojFilesExcludingDirectories(string rootDirectory)
+    {
+        var stack = new Stack<string>();
+        stack.Push(rootDirectory);
+
+        while (stack.Count > 0)
+        {
+            var currentDirectory = stack.Pop();
+            var directoryName = Path.GetFileName(currentDirectory);
+
+            if (DirectoryExclusions.Any(excludedDirectory => string.Equals(directoryName, excludedDirectory, StringComparison.OrdinalIgnoreCase)))
+                continue;
+
+            foreach (var file in Directory.GetFiles(currentDirectory, "*.csproj"))
+                yield return file;
+
+            foreach (var dir in Directory.GetDirectories(currentDirectory))
+                stack.Push(dir);
+        }
     }
 
     // TODO: Domain events should be sealed
